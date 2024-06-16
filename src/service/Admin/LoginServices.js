6b9b1b1eec22563f920c231889fd71129d95148d
Aswinraj <aswinrajr07@gmail.com>
2024-06-17 00:27:12 +0530

    yml removed

--- src/service/Admin/LoginServices.js
+++ src/service/Admin/LoginServices.js
@@ -10,12 +10,12 @@ export const adminLogin = async (email, password) => {
     });
     console.log("...........................................")
     console.log("response", response);
-    console.log(" response.data", response.data);
+    console.log(" response.data", response.data);   
     console.log("response.data.msg", response.data.msg);
     console.log("........................................")
     return response
   } catch (err) {
-    console.log("Error in admin login", err);
+    console.log("Error in admin login", err);  
     return err
-  }
+  } 
 };
