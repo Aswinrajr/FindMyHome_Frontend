c18dbd3ae81af208fde7d9bcd8e0fb4c49fe3e79
Aswinraj <aswinrajr07@gmail.com>
2024-06-18 16:48:33 +0530

    Admin user Provider ui done

--- src/components/Sample/SearchRooms.jsx
+++ src/components/Sample/SearchRooms.jsx
@@ -2,12 +2,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router";
+import { ClipLoader } from "react-spinners";
 
 const SearchRooms = () => {
   const baseRoute = import.meta.env.VITE_BASE_URL_ROUTE;
 
-  const [date, setDate] = useState();
+
   const [error, setError] = useState();
+  const [loading, setLoading] = useState(false); 
+  const [cityName,setCityName] = useState()
 
   const currentDate = new Date();
   const year = currentDate.getFullYear();
@@ -40,15 +43,17 @@ const SearchRooms = () => {
         );
         console.log(cityComponent);
 
-        const cityName = cityComponent ? cityComponent.long_name : "";
+        const cityNames = cityComponent ? cityComponent.long_name : "";
 
-        console.log(cityName);
+        console.log(cityNames);
+        setCityName(cityNames)
       }
     });
   }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    console.log("Formdata",formData)
     if (
       !formData.city ||
       !formData.checkIn ||
@@ -63,7 +68,7 @@ const SearchRooms = () => {
       return;
     }
 
-    if (formData.checkIn > formData.checkOut) {
+    if (formData.checkIn >= formData.checkOut) {
       setError("Selected date is invalid");
       // toast.error("Selected date is invlid");
       setTimeout(() => {
@@ -72,10 +77,10 @@ const SearchRooms = () => {
       return;
     }
 
-    if (formData.checkIn < formattedDate || formData.checkOut < formattedDate) {
+    if (formData.checkIn <= formattedDate || formData.checkOut <= formattedDate) {
       setError("Selected date is invalid");
 
-      // toast.error("Selected date is invalid");
+   
       setTimeout(() => {
         setError("");
       }, 1000);
@@ -84,6 +89,7 @@ const SearchRooms = () => {
     console.log(formData);
 
     try {
+      setLoading(true); 
       const cordResponse = await axios.get(
         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
           formData.city
@@ -118,14 +124,18 @@ const SearchRooms = () => {
           navigate("/searchedroom", { state: data });
         }
       };
-      fetchData();
+      await fetchData();
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false); 
     }
   };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
+    console.log()
+
     if (name === "adults" && value < 1) {
       setError("No of person cannot be less than 1");
       setTimeout(() => {
@@ -140,85 +150,89 @@ const SearchRooms = () => {
       }, 1000);
       return;
     }
+   
 
     setFormData({
       ...formData,
       [name]: value,
     });
+
   };
+  formData.city=cityName
 
   return (
-<section className="py-16 px-4 md:px-8 bg-blue-50">
-  <Toaster position="top-center" reverseOrder={false} />
-  <div className="max-w-4xl mx-auto">
-    <h2 className="text-3xl font-bold mb-8 text-blue-800 text-center">
-      Search Rooms
-    </h2>
-    <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <input
-          type="text"
-          id="city"
-          name="city"
-          placeholder="City"
-          className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500"
-          onChange={handleChange}
-        />
-        <div className="grid grid-cols-2 gap-4">
-          <input
-            type="text"
-            name="checkIn"
-            className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500"
-            onChange={handleChange}
-            placeholder="Check-in Date"
-            onFocus={(e) => (e.target.type = "date")}
-            onBlur={(e) => (e.target.type = e.target.value ? "date" : "text")}
-          />
-          <input
-            type="text"
-            name="checkOut"
-            className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500"
-            onChange={handleChange}
-            placeholder="Check-out Date"
-            onFocus={(e) => (e.target.type = "date")}
-            onBlur={(e) => (e.target.type = e.target.value ? "date" : "text")}
-          />
-        </div>
+    <section className="py-16 px-4 md:px-8 bg-blue-50">
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="max-w-4xl mx-auto">
+        <h2 className="text-3xl font-bold mb-8 text-blue-800 text-center">
+          Search Rooms
+        </h2>
+        <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <input
+              type="text"
+              id="city"
+              name="city"
+              placeholder="City"
+              className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500"
+              onChange={handleChange}
+            />
+            <div className="grid grid-cols-2 gap-4">
+              <input
+                type="text"
+                name="checkIn"
+                className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500"
+                onChange={handleChange}
+                placeholder="Check-in Date"
+                onFocus={(e) => (e.target.type = "date")}
+                onBlur={(e) => (e.target.type = e.target.value ? "date" : "text")}
+              />
+              <input
+                type="text"
+                name="checkOut"
+                className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500"
+                onChange={handleChange}
+                placeholder="Check-out Date"
+                onFocus={(e) => (e.target.type = "date")}
+                onBlur={(e) => (e.target.type = e.target.value ? "date" : "text")}
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <input
+              type="number"
+              name="adults"
+              placeholder="Adults"
+              className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500 appearance-none"
+              min="1"
+              max="10"
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              name="children"
+              placeholder="Children"
+              className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500 appearance-none"
+              min="0"
+              max="10"
+              onChange={handleChange} 
+            />
+          </div>
+          {error && (
+            <p className="text-red-500 text-lg mt-4 bg-red-100 px-4 py-2 rounded-md">
+              {error}
+            </p>
+          )}
+          <button
+            type="submit"
+            className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300"
+            disabled={loading}
+          >
+            {loading ? <ClipLoader size={20} color="#fff" /> : "Search"} 
+          </button>
+        </form>
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <input
-          type="number"
-          name="adults"
-          placeholder="Adults"
-          className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500 appearance-none"
-          min="1"
-          max="10"
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          name="children"
-          placeholder="Children"
-          className="w-full p-2 border border-gray-300 rounded-md focus:border-blue-500 focus:ring-blue-500 appearance-none"
-          min="0"
-          max="10"
-          onChange={handleChange}
-        />
-      </div>
-      {error && (
-        <p className="text-red-500 text-lg mt-4 bg-red-100 px-4 py-2 rounded-md">
-          {error}
-        </p>
-      )}
-      <button
-        type="submit"
-        className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300"
-      >
-        Search
-      </button>
-    </form>
-  </div>
-</section>
+    </section>
   );
 };
 
